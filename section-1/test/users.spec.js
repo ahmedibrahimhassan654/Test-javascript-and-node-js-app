b9ae16c5685545a79522b598d8922b56ce4b7c2b
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2021-04-01 14:56:00 +0200

    7. Code coverage

--- section-1/test/users.spec.js
+++ section-1/test/users.spec.js
@@ -35,3 +35,20 @@ describe('findUserByEmail function',()=>{
      })
 
 }) 
+
+
+describe('findUserById function',()=>{
+    it('find user by Id using async await ',async()=>{
+        const response=await findUserById(2)
+        assert.equal(response.message,'User found successfully.')
+             
+         
+     })
+     it('rejects with error if user with id was not found',()=>{
+        return findUserById(92).then(()=>{
+            assert.fail('expected findUserById function to rejected')
+        },error=>{
+           assert.equal(error.message,'User with id: 92 was not found.')
+        })
+    })
+}) 
